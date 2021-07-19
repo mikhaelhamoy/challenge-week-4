3cbce309e3c0ff4841bdb0ffd08b03f9264a1062
Mikhael Hamoy <mikhaelhamoy@gmail.com>
2021-07-18 23:22:43 -0400

    unfinished

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# challenge-week-4
-Coding Quiz Challenge
+# challenge-week-4 (Coding Quiz Challenge)
+
+## Purpose
+
+Create a coding quiz challenge
+
+## Built With
+
+    * HTML
+    * CSS
+    * JavaScript
+
+## Website
+
+https://mikhaelhamoy.github.io/challenge-week-3/
+
+## Screenshots
+
+## Contribution
+
+Made by Mikhael Hamoy
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var counter = 75;
 var timer;
 var questionNumber = 0;
+var finalScore = 0;
+var highScoreArr = [];
 var questionsArr = ["Commonly used data types DO Not Include:", "The condition in an if / else statement is enclosed with ________.", "Arrays in JavaScript can be used to store _________.", "String values must be enclosed within ______ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
 var choicesArr = [["strings", "booleans","alerts","numbers"],["quotes", "curly brackets", "parenthesis", "square brackets"],["numbers and strings", "other arrays", "booleans", "all of the above"],["commas", "curly brackets", "quotes", "parenthesis"],["JavaScript", "terminal/bash", "for loops", "console.log"]];
 var answersArr = ["alerts", "parenthesis", "all of the above", "quotes", "console.log"];
@@ -9,11 +11,15 @@ var linkTimerEl = document.querySelector("header");
 var questionsOrTitlePageContainerEl = document.querySelector("#questions-page-title");
 var stageContainerEl = document.querySelector(".stage");
 var checkerContainerEl = document.querySelector("#checker");
+var headerEl = document.querySelector("header");
+var formEl = document.createElement("form");
+var linkToHighScorePage = document.getElementById("high-score-link");
 
 
 // function to create the startup page
 var createIntroPage = function(){
     removeContents();
+    showHeaderContents();
 
     // create game title header element
     var gameTitleEl = document.createElement("h3");
@@ -54,9 +60,11 @@ var startTimer = function() {
 var displayTimer = function(){ 
     document.getElementById("timer").textContent = counter;
     console.log(counter);
-    if(counter === 0){
+    if(counter <= 0){
         clearInterval(timer);
+        finalScore = counter;
         counter = 75;
+        gameOver();
     }
     counter--;
 };
@@ -70,21 +78,28 @@ var startCodingQuiz = function(){
 
 // function to generate questions
 var generateQuestions = function() {
-
-    var questionEl = document.createElement("h3");
-    questionEl.textContent = questionsArr[questionNumber];
-    questionEl.className = "align-left";
-    questionsOrTitlePageContainerEl.appendChild(questionEl);
-
-    stageContainerEl.id = "multiple-choice";
-    var multipleChoiceEl = document.createElement("ul");
-    multipleChoiceEl.id = "choices";
-
-    for (var i = 0; i < 4; i++){
-        multipleChoiceEl.appendChild(generateMultipleChoices(questionNumber, i));
+    if (questionNumber === 5){
+        finalScore = counter;
+        clearInterval(timer);
+        questionNumber = 0;
+        gameOver();
     }
+    else {
+        var questionEl = document.createElement("h3");
+        questionEl.textContent = questionsArr[questionNumber];
+        questionEl.className = "align-left";
+        questionsOrTitlePageContainerEl.appendChild(questionEl);
 
-    stageContainerEl.appendChild(multipleChoiceEl);
+        stageContainerEl.id = "multiple-choice";
+        var multipleChoiceEl = document.createElement("ul");
+        multipleChoiceEl.id = "choices";
+
+        for (var i = 0; i < 4; i++){
+            multipleChoiceEl.appendChild(generateMultipleChoices(questionNumber, i));
+        }
+
+        stageContainerEl.appendChild(multipleChoiceEl);
+    }   
 };
 
 var btnClickedHandler = function(event){
@@ -97,11 +112,75 @@ var btnClickedHandler = function(event){
     if(btnClicked.className === "choice"){
         checkAnswer(btnClicked.textContent);
     }
-}
+
+    if (btnClicked.id === "go-back"){
+        createIntroPage();
+    }
+    
+    if(btnClicked.id === "clear-high-scores"){
+        highScoreArr = [];
+        saveHighScores();
+        getHighScores();
+        loadHighScoresPage();
+    }
+};
+
+var submitInitialsHandler =  function(event) {
+    event.preventDefault();
+
+    removeChecker();
+    var initialsInput = document.querySelector("input[name='initials']").value;
+
+    // check if inputs are empty (validate)
+    if (!initialsInput) {
+        alert("You need to fill out the initials form!");
+        gameOver();
+    }
+    else{
+
+        var highScoreDataObj = {
+            initials: initialsInput,
+            score: finalScore
+        };
+        console.log(highScoreArr);
+    
+        if (highScoreArr === []){
+            highScoreArr.push(highScoreDataObj);
+        }
+        else {
+            for (var i = 0; i < highScoreArr.length; i++){
+                if (highScoreDataObj.score > highScoreArr[i].score){
+                    highScoreArr.splice(i, 0, highScoreDataObj);
+                }
+                else if (i === (highScoreArr.length - 1)) {
+                    highScoreArr.push(highScoreDataObj);
+                }
+            }
+        }
+        console.log(highScoreArr);
+
+        saveHighScores();
+        loadHighScoresPage();
+    }
+};
+
+var saveHighScores = function() {
+    localStorage.setItem("highScores", JSON.stringify(highScoreArr));
+};
+
+var getHighScores =  function() {
+    var savedHighScores = localStorage.getItem("highScores");
+
+    if (!savedHighScores) {
+        return false;
+    }
+
+    highScoreArr = JSON.parse(savedHighScores);
+};
 
 var removeContents = function() {
     // remove question or page title
-    while (questionsOrTitlePageContainerEl.hasChildNodes()){
+    if (questionsOrTitlePageContainerEl.hasChildNodes()){
         questionsOrTitlePageContainerEl.removeChild(questionsOrTitlePageContainerEl.lastChild);
     }
 
@@ -109,6 +188,10 @@ var removeContents = function() {
     while (stageContainerEl.hasChildNodes()){
         stageContainerEl.removeChild(stageContainerEl.lastChild);
     } 
+
+    while (formEl.hasChildNodes()){
+        formEl.removeChild(formEl.lastChild);
+    }
 };
 
 var removeChecker = function(){
@@ -117,13 +200,22 @@ var removeChecker = function(){
     }
 };
 
+var hideHeaderContents = function() {
+    document.getElementById("high-score-link").style.visibility = "hidden";
+    document.getElementById("timer-label").style.visibility = "hidden";
+};
+
+var showHeaderContents = function() {
+    document.getElementById("high-score-link").style.visibility = "visible";
+    document.getElementById("timer-label").style.visibility = "visible";
+};
+
 // function to generate the choices
 var generateMultipleChoices = function(qNum, cNum) {
     var choiceEl = document.createElement("li");
     var choiceBtnEl = document.createElement("button");
     choiceBtnEl.textContent = choicesArr[qNum][cNum];
     choiceBtnEl.className = "choice";
-    //debugger;
     choiceEl.appendChild(choiceBtnEl);
     return choiceEl;
 };
@@ -148,6 +240,94 @@ var checkAnswer = function(answer){
     generateQuestions();
 };
 
+var gameOver = function() {
+    removeContents();
 
+    var allDoneEl = document.createElement("h3");
+    allDoneEl.textContent = "All done!";
+    allDoneEl.className = "align-left";
+    questionsOrTitlePageContainerEl.appendChild(allDoneEl);
+
+    stageContainerEl.id = "high-scores";
+
+    var finalScoreEl = document.createElement("p");
+    finalScoreEl.textContent = "Your final score is " + finalScore + ".";
+    stageContainerEl.appendChild(finalScoreEl);
+
+    var initialsFormLabelEl = document.createElement("label");
+    initialsFormLabelEl.setAttribute("for", "initials");
+    initialsFormLabelEl.innerHTML =  "Enter initials:<input type='text' id='initials' name='initials' />";
+
+    formEl.appendChild(initialsFormLabelEl);
+    
+    var submitBtnEl = document.createElement("button");
+    submitBtnEl.textContent = "Submit";
+    submitBtnEl.type = "submit";
+    formEl.appendChild(submitBtnEl);
+
+    stageContainerEl.appendChild(formEl);
+};
+
+var loadHighScoresPage = function() {
+    hideHeaderContents();
+    removeContents();
+    
+    var highScorePageTitleEl =  document.createElement("h3");
+    highScorePageTitleEl.textContent = "High scores";
+    highScorePageTitleEl.className = "align-left";
+    questionsOrTitlePageContainerEl.appendChild(highScorePageTitleEl);
+
+    stageContainerEl.id = "high-scores";
+
+    
+
+    var highScoresContainerEl = document.createElement("div");
+    highScoresContainerEl.className = "score odd-num";
+    highScoresContainerEl.innerHTML = "<p>" + (1+1) + "." + highScoreArr.initials + " - " + highScoreArr.score + "</p>"; 
+    stageContainerEl.appendChild(highScoresContainerEl);
+    
+    // for (var i = 0; i < highScoreArr.length; i++){
+    //     var modResult = i % 2;
+    //     if (modResult === 0 || modResult === undefined){
+            
+    //         highScoresContainerEl.className = "score odd-num";
+    //         highScoresContainerEl.innerHTML = "<p>" + (i+1) + "." + highScoreArr[i].initials + " - " + highScoreArr[i].score + "</p>"; 
+    //         stageContainerEl.appendChild(highScoresContainerEl);
+    //     }
+    //     else {
+    
+    //         highScoresContainerEl.className = "score";
+    //         highScoresContainerEl.innerHTML = "<p>" + (i+1) + "." + highScoreArr[i].initials + " - " + highScoreArr[i].score + "</p>"; 
+    //         stageContainerEl.appendChild(highScoresContainerEl);
+    //     }
+    // }
+
+    var highScorePageBtnContainerEl = document.createElement("div");
+    var goBackBtnEl = document.createElement("button");
+    goBackBtnEl.textContent = "Go Back";
+    goBackBtnEl.id = "go-back";
+    highScorePageBtnContainerEl.appendChild(goBackBtnEl);
+
+    var clearHighScoreBtnEl = document.createElement("button");
+    clearHighScoreBtnEl.textContent = "Clear High Scores";
+    clearHighScoreBtnEl.id = "clear-high-scores";
+    highScorePageBtnContainerEl.appendChild(clearHighScoreBtnEl);
+
+    stageContainerEl.appendChild(highScorePageBtnContainerEl);
+
+    console.log(stageContainerEl);
+};
+
+var useHighScoreLink = function() {
+    clearInterval(timer);
+    counter = 75;
+    questionNumber = 0;
+    loadHighScoresPage();
+};
+
+
+getHighScores();
 createIntroPage();
 stageContainerEl.addEventListener("click", btnClickedHandler);
+formEl.addEventListener("submit", submitInitialsHandler);
+linkToHighScorePage.addEventListener("click", useHighScoreLink);
